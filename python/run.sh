2be4715c238404c9ce039eba42ec38ee04fe3668
Martijn Donath <martijn.donath@gmail.com>
2024-01-20 22:55:05 +0100

    Updating requirements

--- python/run.sh
+++ python/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "stap 1"
 source venv/bin/activate
-
+echo "stap 2"
 /home/pi/git/openid-sn/python/venv/bin/python3 /home/pi/git/openid-sn/python/update_kampinfo.py >> /home/pi/git/openid-sn/python/output.log
+echo "stap 3"
